034f9be7bf57160c0b0731059aa66a068db95ae3
Kalle-KO <kaot0001@stud.kea.dk>
2024-10-21 09:30:54 +0200

    opdateret kode med controller, repo og app

--- turist-master/src/main/java/Tourism/Repository/TouristRepository.java
+++ turist-master/src/main/java/Tourism/Repository/TouristRepository.java
@@ -17,7 +17,7 @@ public class TouristRepository {
     List<String> tags = new ArrayList<>();
     String database = "jdbc:mysql://localhost:3306/tourist_attractions";
     String username = "root";
-    String password = "Fam39ash012002";
+    String password = "Illcosby91";
     Connection conn;
 
     public TouristRepository(){
@@ -99,6 +99,51 @@ public class TouristRepository {
         return null;
     }
 
+    /**
+     * CRUD method to read an attraction from the database, based on input name
+     * @param name input to search for database entry
+     * @return a corresponding tourist attraction
+     */
+    public TouristAttraction readAttractionByName(String name){
+        String descriptionQuery = "SELECT * FROM attraction_description WHERE name = ?;";
+        String tagsQuery = "SELECT * FROM attraction_tags WHERE name = ?;";
+        try{
+            ArrayList<TouristAttraction> tempList = new ArrayList<>();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(database, username, password);
+            if (conn == null) {
+                System.out.println("connection not established.");
+            }
+            PreparedStatement pstmtDescription = conn.prepareStatement(descriptionQuery);
+            pstmtDescription.setString(1,name);
+            ResultSet rsDescription = pstmtDescription.executeQuery();
+            while(rsDescription.next()){
+                String attractionName = rsDescription.getString("name");
+                String attractionDescription = rsDescription.getString("description");
+                String attractionCity = rsDescription.getString("city");
+                ArrayList<String> attractionTags = new ArrayList<>();
+                TouristAttraction touristAttraction = new TouristAttraction(attractionName,attractionDescription,attractionCity,attractionTags);
+                tempList.add(touristAttraction);
+            }
+            PreparedStatement pstmtTags = conn.prepareStatement(tagsQuery);
+            pstmtTags.setString(1,name);
+            ResultSet rsTags = pstmtTags.executeQuery();
+            while(rsTags.next()){
+                ArrayList<String> attractionTags = new ArrayList<>();
+                attractionTags.add(rsTags.getString("tag1"));
+                attractionTags.add(rsTags.getString("tag2"));
+                attractionTags.add(rsTags.getString("tag3"));
+                attractionTags.add(rsTags.getString("tag4"));
+                tempList.get(0).setTags(attractionTags);
+            }
+            return tempList.get(0);
+        }
+        catch (Exception e){
+            e.getMessage();
+        }
+        return null;
+    }
+
 
     /**
      * CRUD method to remove a tourist attraction from the list
@@ -146,6 +191,79 @@ public class TouristRepository {
         return null;
     }
 
+    /**
+     * CRUD method to create a new tourist attraction
+     * @param touristAttraction the attraction to be created
+     */
+    public void saveAttraction(TouristAttraction touristAttraction){
+        getTouristAttractionList().add(touristAttraction);
+        String attractionName = touristAttraction.getName();
+        String attractionDescription = touristAttraction.getDescription();
+        String attractionCity = touristAttraction.getCity();
+        List<String> attractionTags = touristAttraction.getTags();
+        String saveQueryDescription = "INSERT INTO `attraction_description` (`name`,`description`,`city`)  VALUES (?,?,?);";
+        String saveQueryTags = "INSERT INTO `attraction_tags` (`name`,`city`,`tag1`,`tag2`,`tag3`,`tag4` ) VALUES (?,?,?,?,?,?);";
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(database, username, password);
+            if (conn == null) {
+                System.out.println("connection not established.");
+            }
+            PreparedStatement psmtSaveDescription = conn.prepareStatement(saveQueryDescription);
+            psmtSaveDescription.setString(1,attractionName);
+            psmtSaveDescription.setString(2,attractionDescription);
+            psmtSaveDescription.setString(3,attractionCity);
+            PreparedStatement psmtSaveTags = conn.prepareStatement(saveQueryTags);
+            psmtSaveTags.setString(1,attractionName);
+            psmtSaveTags.setString(2,attractionCity);
+            psmtSaveTags.setString(3,attractionTags.get(0));
+            psmtSaveTags.setString(4,attractionTags.get(1));
+            psmtSaveTags.setString(5,attractionTags.get(2));
+            psmtSaveTags.setString(6,attractionTags.get(3));
+
+            psmtSaveDescription.executeUpdate();
+            psmtSaveTags.executeUpdate();
+
+        }
+        catch (Exception e){
+            System.out.println("Error: Could not insert attraction" + e.getMessage());
+        }
+    }
+
+    /**
+     * CRUD method to update an attraction
+     * @param touristAttraction The tourist attraction to be updated
+     * @param name Finds the given attraction by name
+     */
+    public void updateAttraction(TouristAttraction touristAttraction, String name) {
+        String updateDescriptionQuery = "UPDATE `attraction_description` SET name = ?, description = ?, city = ? WHERE name = ?;";
+        String updateTagsQuery = "UPDATE attraction_tags SET tag1 = ?, tag2 = ?, tag3 = ?, tag4 = ? WHERE name = ?;";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(database, username, password);
+            if (conn == null) {
+                System.out.println("connection not established.");
+            }
+            PreparedStatement psmtUpdateDescription = conn.prepareStatement(updateDescriptionQuery);
+            psmtUpdateDescription.setString(1, touristAttraction.getName());
+            psmtUpdateDescription.setString(2,touristAttraction.getDescription());
+            psmtUpdateDescription.setString(3,touristAttraction.getCity());
+            psmtUpdateDescription.setString(4,name);
+            psmtUpdateDescription.executeUpdate();
+            PreparedStatement psmtUpdateTags = conn.prepareStatement(updateTagsQuery);
+            psmtUpdateTags.setString(1,touristAttraction.getTags().get(0));
+            psmtUpdateTags.setString(2,touristAttraction.getTags().get(1));
+            psmtUpdateTags.setString(3,touristAttraction.getTags().get(2));
+            psmtUpdateTags.setString(4,touristAttraction.getTags().get(3));
+            psmtUpdateTags.setString(5, name);
+            psmtUpdateTags.executeUpdate();
+        }
+        catch (Exception e){
+            e.getMessage();
+        }
+    }
+
     public List<String> getCities(){
         return cities;
     }
@@ -153,6 +271,7 @@ public class TouristRepository {
     public List<String> getTags(){
         return tags;
     }
+
 
 
 }
